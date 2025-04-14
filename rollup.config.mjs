c54a2e7d51e15a740ef978dc47817e06972358ce
Micropue <wangyitong0818@icloud.com>
2025-04-14 09:21:55 +0800

    添加可读版iife打包代码.

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,12 +1,31 @@
 /** @type {import('rollup').RollupOptions} */
+/** 
+ * @type {import('@rollup/plugin-terser').terser}
+ */
 import terser from '@rollup/plugin-terser'
 export default {
     input: "./dist/main.js",
-    output: {
-        file: "./dist/monphind.min.js",
-        format: "iife",
-        name: "monphind",
-        compact: true
-    },
-    plugins: [terser()]
+    output: [
+        {
+            file: "./dist/monphind.min.js",
+            format: "iife",
+            name: "monphind",
+            compact: true,
+            plugins: [terser()]
+        },
+        {
+            file: "./dist/monphind.js",
+            format: "iife",
+            name: "monphind",
+            compact: true,
+            plugins:[terser({
+                // mangle:{
+                //     eval:true
+                // },
+                format:{
+                    semicolons:false
+                }
+            })]
+        }
+    ],
 }
